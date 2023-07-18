062638d1073ec3e509380e09d78f5880ce0f09bb
ghwntksanfaldur <tmdqts55@naver.com>
2023-07-18 09:49:52 +0900

    first commit

---
+++ app/src/main/java/com/example/a230718_android_lab_pra/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a230718_android_lab_pra
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
